a83db9dae88f04b9fedc49a529a99cee3fb6de61
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-02-16 12:56:09 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'viewChild';
+export class AppComponent implements AfterViewInit {
+
+  // create a variable of type ElementRef
+  @ViewChild('nameRef')nameTypeRef: ElementRef;
+
+  // access if on template render
+  // focus on field when page is rendered
+ngAfterViewInit(){
+  this.nameTypeRef.nativeElement.focus();
+}
 }
